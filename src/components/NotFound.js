29d17a4266c779f9a0e4eb6bf358ddb2257657a0
Natalia <atvnataliajn@gmail.com>
2022-01-27 10:31:56 +0100

    update styles character not found

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
-const NotFound= () => {
-return(
+import "../styles/components/NotFound.scss";
+const NotFound = () => {
+  return (
     <>
-    <Link to="/">Volver a la página principal</Link>
-    <p>Personaje no encontrado</p>
-  
+      <Link className="homeLink" to="/">Volver a Howarts</Link>
+      <div className="characterNotFoundContainer" >
+          <div className="characterNotFoundContainer__textContainer">
+        <p className="characterNotFoundContainer__textContainer--paragraph1">No hemos encontrado ese personaje, puede que no exista o que se haya perdido por Howarts... </p>
+        <p className="characterNotFoundContainer__textContainer--paragraph2">Puedes buscarlo en el Mapa del Merodeador o volver a Howarts e intentarlo de nuevo</p></div>
+      </div>
     </>
-)
+  );
 };
 
 export default NotFound;
