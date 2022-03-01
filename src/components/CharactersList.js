a985906409c5b959b8b1fdae35d268686d7ff186
Natalia <atvnataliajn@gmail.com>
2022-03-01 01:07:39 +0100

    add hover flip card

--- src/components/CharactersList.js
+++ src/components/CharactersList.js
@@ -1,16 +1,27 @@
 import "../styles/components/characters/CharactersList.scss";
 import CharacterCard from "./CharacterCard";
+import hogwartsShield from "../images/hogwartsShield.png";
 
-const CharactersList = ({charactersData}) => {
-  const characterElements = charactersData.map(
-    (eachCharacterData, index) => {
-      return (
-        <li className="charactersListContainer__charactersList--item" key={index}>
+const CharactersList = ({ charactersData }) => {
+  const characterElements = charactersData.map((eachCharacterData, index) => {
+    return (
+      <div className="hoverContainer">
+        <li
+          className="charactersListContainer__charactersList--item front "
+          key={index}
+        >
           <CharacterCard eachCharacterData={eachCharacterData} />
         </li>
-      );
-    }
-  );
+        <li className="back">
+          <img
+            className="back__hogwartsShield"
+            alt="Escudo de Hogwarts"
+            src={hogwartsShield}
+          />
+        </li>
+      </div>
+    );
+  });
 
   return (
     <section className="charactersListContainer">
